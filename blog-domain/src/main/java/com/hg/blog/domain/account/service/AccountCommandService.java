658287778cdad4ee1dae309c00f409c5fbcecaa3
hyungyu <yhg8484@naver.com>
2022-08-07 15:21:50 +0900

    feat(domain): account command service 추가 및 account 생성 테스트 코드 작성

---
+++ blog-domain/src/main/java/com/hg/blog/domain/account/service/AccountCommandService.java
@@ -0,0 +1,27 @@
+package com.hg.blog.domain.account.service;
+
+import com.hg.blog.domain.account.entity.Account;
+import com.hg.blog.domain.account.entity.AccountRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class AccountCommandService {
+
+    private final AccountRepository accountRepository;
+
+    @Transactional
+    public Account addAccount(Account account) {
+        checkExistAccount(account.getUserId());
+        return accountRepository.save(account);
+    }
+
+    private void checkExistAccount(String userId) {
+        accountRepository.findByUserId(userId)
+            .ifPresent(findAccount -> {
+                throw new IllegalArgumentException("이미 존재하는 ID 입니다.");
+            });
+    }
+}
---
+++ blog-domain/src/test/java/com/hg/blog/domain/account/service/AccountCommandServiceTest.java
@@ -0,0 +1,57 @@
+package com.hg.blog.domain.account.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import com.hg.blog.domain.account.entity.Account;
+import com.hg.blog.domain.account.entity.AccountRepository;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AccountCommandServiceTest {
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @InjectMocks
+    private AccountCommandService accountCommandService;
+
+    @Test
+    public void add_account_test() {
+        // given
+        Account account = createAccount();
+        given(accountRepository.save(any())).willReturn(account);
+
+        // when
+        Account savedAccount = accountCommandService.addAccount(account);
+
+        // then
+        assertThat(savedAccount).isNotNull();
+        assertThat(savedAccount.getUserId()).isEqualTo("userid");
+        assertThat(savedAccount.getPassword()).isEqualTo("password");
+        assertThat(savedAccount.getNickname()).isEqualTo("nickname");
+    }
+
+    @Test
+    public void add_account_exist_error_test() {
+        // given
+        Account account = createAccount();
+        given(accountRepository.findByUserId(account.getUserId())).willReturn(Optional.of(account));
+        // when
+        Executable execute = () -> accountCommandService.addAccount(account);
+        // then
+        assertThrows(IllegalArgumentException.class, execute);
+    }
+
+    private Account createAccount() {
+        return Account.of("userid", "password", "nickname");
+    }
+}
