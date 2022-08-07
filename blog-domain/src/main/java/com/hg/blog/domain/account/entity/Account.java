ec87987d67ceafb40fad100e7406b76c6f3255b3
hyungyu <yhg8484@naver.com>
2022-08-07 15:19:36 +0900

    feat(account): account domain 생성

---
+++ blog-domain/src/main/java/com/hg/blog/domain/account/entity/Account.java
@@ -0,0 +1,50 @@
+package com.hg.blog.domain.account.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Account {
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Account(String userId, String password, String nickname) {
+        this.userId = userId;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    private String userId;
+
+    private String password;
+
+    private String nickname;
+
+    @Column(updatable = false)
+    private LocalDateTime created = LocalDateTime.now();
+
+    @Column(updatable = false)
+    private LocalDateTime updated = LocalDateTime.now();
+
+    private boolean deletedYn;
+
+    public static Account of(String userId, String password, String nickname) {
+        return Account.builder()
+            .userId(userId)
+            .password(password)
+            .nickname(nickname)
+            .build();
+    }
+}
