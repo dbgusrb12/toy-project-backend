87469c29c95b448b865010d36311779b967d2202
hyungyu <yhg8484@naver.com>
2022-08-07 17:31:54 +0900

    fix(application): ExceptionResolver.java HttpStatus 추가

--- blog-application/src/main/java/com/hg/blog/exception/ExceptionResolver.java
+++ blog-application/src/main/java/com/hg/blog/exception/ExceptionResolver.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
@@ -12,12 +13,14 @@ import org.springframework.web.context.request.WebRequest;
 public class ExceptionResolver {
 
     @ExceptionHandler(value = IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse illegalArgumentExceptionHandler(HttpServletRequest request,
         IllegalArgumentException ex) {
         return ErrorResponse.of(HttpStatus.BAD_REQUEST, ex);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse methodArgumentNotValidException(MethodArgumentNotValidException ex, WebRequest request) {
         String message = ex.getBindingResult().getFieldErrors().stream()
             .map(exception -> String.format("[%s](은)는 %s. 입력된 값: [%s]", exception.getField(),
