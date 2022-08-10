baf1ccb6cffd84bbbdffeca16cf7286fa52d88bb
hyungyu <yhg8484@naver.com>
2022-08-10 19:23:31 +0900

    feat: signup test 로직 추가

--- blog-application/src/test/java/com/hg/blog/api/account/service/AccountServiceTest.java
+++ blog-application/src/test/java/com/hg/blog/api/account/service/AccountServiceTest.java
@@ -1,10 +1,12 @@
 package com.hg.blog.api.account.service;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 import com.hg.blog.api.account.dto.AccountDto;
 import com.hg.blog.domain.account.service.AccountCommandService;
+import com.hg.blog.util.RSAUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -17,13 +19,21 @@ public class AccountServiceTest {
     @Mock
     private AccountCommandService accountCommandService;
 
+    @Mock
+    private RSAUtil rsaUtil;
+
     @InjectMocks
     private AccountService accountService;
 
     @Test
     public void signUpTest() {
+        // given
         AccountDto.SignUpCommand signUpCommand = createSignUpCommand();
+        given(rsaUtil.decrypt(any()))
+            .willReturn("password");
+        // when
         accountService.signUp(signUpCommand);
+        // then
         verify(accountCommandService).addAccount(any());
     }
 
