25735f745b799ce2ec5111f5749566f970ddd7d9
hyungyu <yhg8484@naver.com>
2022-08-13 21:14:21 +0900

    lint(test): account service test lint 수정

--- blog-application/src/test/java/com/hg/blog/api/account/service/AccountServiceTest.java
+++ blog-application/src/test/java/com/hg/blog/api/account/service/AccountServiceTest.java
@@ -1,7 +1,6 @@
 package com.hg.blog.api.account.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
