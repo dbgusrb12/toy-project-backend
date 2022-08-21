966d06364fbe4fe2089fad8ef5efa251aa25281c
hyungyu <yhg8484@naver.com>
2022-08-21 18:44:21 +0900

    feat(client): naver client feign 적용

--- blog-clients/naver-client/src/main/java/com/hg/blog/feign/FeignConfig.java
+++ blog-clients/naver-client/src/main/java/com/hg/blog/feign/FeignConfig.java
@@ -1,8 +1,16 @@
 package com.hg.blog.feign;
 
+import feign.Logger;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableFeignClients(basePackageClasses = FeignConfig.class)
 public class FeignConfig {
 
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
 }
