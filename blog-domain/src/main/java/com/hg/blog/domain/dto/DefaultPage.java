2c1bfd60c86cc964a135acb0af442189365cc886
hyungyu <yhg8484@naver.com>
2022-09-04 15:44:00 +0900

    feat: custom Page 객체 추가

---
+++ blog-domain/src/main/java/com/hg/blog/domain/dto/DefaultPage.java
@@ -0,0 +1,44 @@
+package com.hg.blog.domain.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import lombok.Getter;
+import org.springframework.data.domain.Page;
+import org.springframework.util.Assert;
+
+@Getter
+public class DefaultPage<T> {
+
+    private final List<T> content = new ArrayList<>();
+    private final long totalElements;
+    private final long totalPages;
+    private final long currentPage;
+
+    private DefaultPage(List<T> content, long totalElements, long totalPages, long currentPage) {
+        this.content.addAll(content);
+        this.totalElements = totalElements;
+        this.totalPages = totalPages;
+        this.currentPage = currentPage;
+    }
+
+    public <U> DefaultPage<U> map(Function<? super T, ? extends U> converter) {
+        return new DefaultPage<>(getConvertedContent(converter), this.totalElements, this.totalPages, this.currentPage);
+    }
+
+    private <U> List<U> getConvertedContent(Function<? super T, ? extends U> converter) {
+        Assert.notNull(converter, "Function must not be null");
+
+        return this.content.stream().map(converter).collect(Collectors.toList());
+    }
+
+    public static <T> DefaultPage<T> of(Page<T> page) {
+        return new DefaultPage<>(
+            page.getContent(),
+            page.getTotalElements(),
+            page.getTotalPages(),
+            page.getPageable().getPageNumber()
+        );
+    }
+}
