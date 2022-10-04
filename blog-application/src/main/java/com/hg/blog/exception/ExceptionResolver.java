56ae6d4cd381c44ebc3cd0239e719c457c679ac7
hyungyu <hg.yu@twolinecode.com>
2022-10-04 22:22:03 +0900

    fix: exception resolver debug 용 로그 추가

--- blog-application/src/main/java/com/hg/blog/exception/ExceptionResolver.java
+++ blog-application/src/main/java/com/hg/blog/exception/ExceptionResolver.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @RestControllerAdvice
 @Slf4j
@@ -24,26 +25,35 @@ public class ExceptionResolver {
     @ExceptionHandler({AuthenticationException.class, AccessDeniedException.class})
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ErrorResponse handleAuthenticationException(RuntimeException ex) {
-        log.error("security error ==> {}", ex.getMessage());
-        log.error("security error ==> {}", ex.getClass().getName());
+        print("security Error: ", ex);
         return ErrorResponse.of(HttpStatus.UNAUTHORIZED, ex);
+    }
+
+    @ExceptionHandler(value = NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(NoHandlerFoundException ex) {
+        print("Not Found Error: ", ex);
+        return ErrorResponse.of(HttpStatus.NOT_FOUND, ex.getMessage());
     }
 
     @ExceptionHandler(value = AccessControlException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ErrorResponse accessControlExceptionHandler(AccessControlException ex) {
+        print("Access Control Error: ", ex);
         return ErrorResponse.of(HttpStatus.UNAUTHORIZED, ex);
     }
 
     @ExceptionHandler(value = IllegalArgumentException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse illegalArgumentExceptionHandler(IllegalArgumentException ex) {
+        print("Illegal Argument Error: ", ex);
         return ErrorResponse.of(HttpStatus.BAD_REQUEST, ex);
     }
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        print("Valid Error: ", ex);
         String message = ex.getBindingResult().getFieldErrors().stream()
             .map(exception ->
                 String.format("[%s](은)는 %s. 입력된 값: [%s]",
@@ -54,5 +64,9 @@ public class ExceptionResolver {
             )
             .collect(Collectors.joining(", "));
         return ErrorResponse.of(HttpStatus.BAD_REQUEST, message);
+    }
+
+    private void print(String format, Exception e) {
+        log.error(format, e);
     }
 }
